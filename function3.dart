c118a3aba4b238c6fb0327d8fbba40a44954e1f7
samson adeyemi <samadeyemi1888@gmail.com>
2022-10-17 10:57:30 -0700

    update sqeunce

--- function3.dart
+++ function3.dart
@@ -1,3 +1,7 @@
-void main(){
-  
+void main() {
+  for (int i = 0; i <= 100000; i++) {
+    if (i % 3 == 1) {
+      print(i);
+    }
+  }
 }
---
+++ function4.dart
@@ -0,0 +1,18 @@
+import 'dart:_js_helper';
+
+void main() {
+
+  favNums(numB)
+}
+
+String favNums(numbs) {
+ Returns(favNums(numB ));List<int> numB = [
+    10,
+    20,
+    30,
+    40,
+    50,
+  ];
+
+  print(favNums(numbs));
+}
