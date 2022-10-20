7d0319ece8f03972b88daa5316ff6cf17718b33a
samson adeyemi <samadeyemi1888@gmail.com>
2022-10-20 07:13:19 -0700

    update squence

--- function2.dart
+++ function2.dart
@@ -1,3 +1,12 @@
+
+// //Write a function that takes a list of strings an prints them, one per line, in a rectangular frame. For example the list ["Hello", "World", "in", "a", "frame"] gets printed as:///
+// ***
+// * Hello *
+// * World *
+// * in    *
+// * a     *
+// * frame *
+// ***
 void main() {
   printdata('bisi,shola, ope');
 }
