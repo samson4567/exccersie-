93142248e4ae4fa38aadf1a0bb53bab0b34464e4
samson adeyemi <samadeyemi1888@gmail.com>
2022-11-25 10:08:48 -0700

    set up

---
+++ function5.dart
@@ -0,0 +1,8 @@
+void main() {
+  //Write a program that prints a multiplication table for numbers up to 12
+  for (int i = 1; i <= 12; i++) {
+    if (i % 1== 12) {
+      print(i);
+    }
+  }
+}
---
+++ function6.dart
@@ -0,0 +1,12 @@
+//Write a function that tests whether a string is a palindrome.
+
+void main() {
+  String name1 = 'dog ';
+  String name2 o= 'god';
+  print(input.split('name1').reversed.join());
+}
+
+void reverse(String name1, String name2) {
+  print(name1);
+  print(name2);
+}
